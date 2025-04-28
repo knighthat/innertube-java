115b71121a8f66183977337dea858c28927b0f33
knighthat <git@knighthat.me>
2025-04-28 00:02:50 -0500

    pre-defined endpoints

---
+++ src/main/java/me/knighthat/innertube/Endpoints.java
@@ -0,0 +1,19 @@
+package me.knighthat.innertube;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Endpoints {
+
+    private Endpoints() {}
+
+    @NotNull
+    public static final String BROWSE = "youtubei/v1/browse";
+    @NotNull
+    public static final String NEXT = "youtubei/v1/browse";
+    @NotNull
+    public static final String PLAYER = "youtubei/v1/browse";
+    @NotNull
+    public static final String SEARCH = "youtubei/v1/browse";
+    @NotNull
+    public static final String SEARCH_SUGGESTIONS = "youtubei/v1/get_search_suggestions";
+}
