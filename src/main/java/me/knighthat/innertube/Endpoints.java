0a79bebb15b9172e19966d681fb7caba413e9db0
knighthat <git@knighthat.me>
2025-04-30 21:29:13 -0500

    correct paths

--- src/main/java/me/knighthat/innertube/Endpoints.java
+++ src/main/java/me/knighthat/innertube/Endpoints.java
@@ -9,11 +9,11 @@ public class Endpoints {
     @NotNull
     public static final String BROWSE = "youtubei/v1/browse";
     @NotNull
-    public static final String NEXT = "youtubei/v1/browse";
+    public static final String NEXT = "youtubei/v1/next";
     @NotNull
-    public static final String PLAYER = "youtubei/v1/browse";
+    public static final String PLAYER = "youtubei/v1/player";
     @NotNull
-    public static final String SEARCH = "youtubei/v1/browse";
+    public static final String SEARCH = "youtubei/v1/search";
     @NotNull
-    public static final String SEARCH_SUGGESTIONS = "youtubei/v1/get_search_suggestions";
+    public static final String SEARCH_SUGGESTIONS = "youtubei/v1/music/get_search_suggestions";
 }
