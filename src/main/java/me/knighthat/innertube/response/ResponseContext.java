693251285a4ad35e3dc2380c2d9f82ed8412fe6b
knighthat <git@knighthat.me>
2025-05-09 20:13:09 -0500

    add service tracking param section in response context

--- src/main/java/me/knighthat/innertube/response/ResponseContext.java
+++ src/main/java/me/knighthat/innertube/response/ResponseContext.java
@@ -1,8 +1,34 @@
 package me.knighthat.innertube.response;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
 public interface ResponseContext {
 
+    @NotNull
     String getVisitorData();
 
+    @NotNull
+    List<? extends ServiceTrackingParam> getServiceTrackingParams();
+
     Integer getMaxAgeSeconds();
+
+    interface ServiceTrackingParam {
+
+        @NotNull
+        String getService();
+
+        @NotNull
+        List<? extends Param> getParams();
+
+        interface Param {
+
+            @NotNull
+            String getKey();
+
+            @NotNull
+            String getValue();
+        }
+    }
 }
--- src/test/java/me/knighthat/impl/response/ResponseContextImpl.java
+++ src/test/java/me/knighthat/impl/response/ResponseContextImpl.java
@@ -1,5 +1,7 @@
 package me.knighthat.impl.response;
 
+import java.util.List;
+
 import lombok.Data;
 import me.knighthat.innertube.response.ResponseContext;
 
@@ -7,5 +9,20 @@ import me.knighthat.innertube.response.ResponseContext;
 public class ResponseContextImpl implements ResponseContext {
 
     private final String visitorData;
+    private final List<ServiceTrackingParamImpl> serviceTrackingParams;
     private final Integer maxAgeSeconds;
+
+    @Data
+    public static class ServiceTrackingParamImpl implements ServiceTrackingParam {
+
+        private final String service;
+        private final List<ParamImpl> params;
+
+        @Data
+        public static class ParamImpl implements Param {
+
+            private final String key;
+            private final String value;
+        }
+    }
 }
