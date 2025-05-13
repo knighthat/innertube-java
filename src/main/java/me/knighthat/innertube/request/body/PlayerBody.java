55949ac508dc546e59171604cd71d44f356a93bf
knighthat <git@knighthat.me>
2025-05-12 21:13:43 -0500

    additional fields for lower chance of failure

--- src/main/java/me/knighthat/innertube/request/body/PlayerBody.java
+++ src/main/java/me/knighthat/innertube/request/body/PlayerBody.java
@@ -9,8 +9,32 @@ public class PlayerBody implements RequestBody {
 
     @NotNull
     private final Context context;
+
     @NotNull
     private final String videoId;
+
     @Nullable
     private final String params;
+
+    @Nullable
+    private final Boolean racyCheckOk;
+
+    @Nullable
+    private final Boolean contentCheckOk;
+
+    /**
+     * Although optional, adding this reduces chances of
+     * blocking significantly, especially on mobile devices.
+     * <p>
+     * A 12-character string is all required.
+     */
+    @Nullable
+    private final ServiceIntegrityDimensions serviceIntegrityDimensions;
+
+    @Data
+    public static class ServiceIntegrityDimensions {
+
+        @NotNull
+        private final String poToken;
+    }
 }
