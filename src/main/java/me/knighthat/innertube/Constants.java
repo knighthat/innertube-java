db7d9c653e565a03d67d46727aa7d05cddba7a2b
knighthat <git@knighthat.me>
2025-06-08 19:50:06 -0500

    add default json headers

--- src/main/java/me/knighthat/innertube/Constants.java
+++ src/main/java/me/knighthat/innertube/Constants.java
@@ -2,8 +2,12 @@ package me.knighthat.innertube;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+import java.util.Map;
+
 public class Constants {
 
+    // START: Static fields/functions
     @NotNull
     public static final String VISITOR_DATA = "CgtMN0FkbDFaWERfdyi8t4u7BjIKCgJWThIEGgAgWQ%3D%3D";
 
@@ -15,6 +19,15 @@ public class Constants {
 
     @NotNull
     public static final String ACCEPT_HEADERS = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+
+    @NotNull
+    public static final Map<String, List<String>> JSON_HEADERS = Map.of(
+            "Content-Type", List.of( "application/json" ),
+            "Accept-Encoding", List.of( "gzip", "deflate" ),
+            "Content-Encoding", List.of( "gzip" ),
+            "User-Agent", List.of( UserAgents.CHROME_WINDOWS )
+    );
+    // END: Static fields/functions
 
     private Constants() {}
 }
