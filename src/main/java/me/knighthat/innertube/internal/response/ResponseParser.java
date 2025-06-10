d6ebb0546c235c446602bca580b9fd8db11491e3
knighthat <git@knighthat.me>
2025-06-10 15:38:41 -0500

    add type adaptor to make sure all lists are not null

--- src/main/java/me/knighthat/innertube/internal/response/ResponseParser.java
+++ src/main/java/me/knighthat/innertube/internal/response/ResponseParser.java
@@ -1,15 +1,23 @@
 package me.knighthat.innertube.internal.response;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import me.knighthat.innertube.response.*;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.List;
+
 public class ResponseParser {
 
-// START: Static fields/functions
+    // START: Static fields/functions
     @NotNull
-    private static final Gson GSON = new Gson();
+    private static final Gson GSON = new GsonBuilder().registerTypeAdapterFactory( new NonNullListProcessor() )
+                                                      .create();
 
     public static BrowseResponse browse( @NotNull Response response ) throws JsonSyntaxException {
         return GSON.fromJson( response.getResponseBody(), BrowseResponseImpl.class );
@@ -30,7 +38,49 @@ public class ResponseParser {
     public static SearchSuggestionsResponse searchSuggestions( @NotNull Response response ) throws JsonSyntaxException {
         return GSON.fromJson( response.getResponseBody(), SearchSuggestionsResponseImpl.class );
     }
-// END: Static fields/functions
+    // END: Static fields/functions
 
     private ResponseParser() { }
+
+    static class NonNullListProcessor implements TypeAdapterFactory {
+
+        @Override
+        public <T> TypeAdapter<T> create( @NotNull Gson gson, @NotNull TypeToken<T> typeToken ) {
+            TypeAdapter<T> delegate = gson.getDelegateAdapter( this, typeToken );
+
+            return new TypeAdapter<T>() {
+                @Override
+                public void write( JsonWriter jsonWriter, T t ) throws IOException {
+                    delegate.write( jsonWriter, t );
+                }
+
+                @Override
+                public T read( JsonReader jsonReader ) throws IOException {
+                    T obj = delegate.read( jsonReader );
+
+                    for ( Field field : obj.getClass()
+                                           .getDeclaredFields() ) {
+                        // Filter out List fields
+                        if ( !List.class.isAssignableFrom( field.getType() ) )
+                            continue;
+
+                        try {
+                            // All fields are private and final
+                            // Setting this to `true` allow modification
+                            field.setAccessible( true );
+
+                            Object fieldValue = field.get( obj );
+                            if ( fieldValue == null )
+                                field.set( obj, Collections.emptyList() );
+                        } catch ( IllegalAccessException e ) {
+                            // This block should never see the light
+                            throw new RuntimeException( e );
+                        }
+                    }
+
+                    return obj;
+                }
+            };
+        }
+    }
 }
