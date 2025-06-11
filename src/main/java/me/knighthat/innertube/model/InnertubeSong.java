d05d6ccbc4139fe1c55de571f0cc89e8950eca3f
knighthat <git@knighthat.me>
2025-06-11 01:21:03 -0500

    move extractArtistsAndAlbum to ItemUtils

--- src/main/java/me/knighthat/innertube/model/InnertubeSong.java
+++ src/main/java/me/knighthat/innertube/model/InnertubeSong.java
@@ -25,55 +25,6 @@ public class InnertubeSong implements Identifiable, Visual {
     private static final Pattern DURATION_PATTERN = Pattern.compile( "^(?:(\\d+):)?(\\d{1,2}):(\\d{2})$" );
 
     /**
-     * Extract album and artists from provided {@link Runs.Run}.
-     * <p>
-     * Album or artist is filtered by their respected {@link PageType}.
-     * <p>
-     * Album's browse will always located at the beginning of the list,
-     * if album browse endpoint doesn't exist in {@code runs}, then
-     * list will be appended with a null {@code null}.
-     * <p>
-     * If the list only contains 1 {@code null } element, it means
-     * the provided {@code runs} contains neither artists nor album
-     *
-     * @param runs set of text and navigation endpoints
-     *
-     * @return album' and artists' browse endpoints in 1 list
-     */
-    private static @NotNull LinkedList<Endpoint.Browse> extractArtistsAndAlbum( @NotNull List<? extends Runs.Run> runs ) {
-        List<Endpoint.Browse> authors = new ArrayList<>();
-        Endpoint.Browse album = null;
-
-        for ( Runs.Run run : runs )
-            try {
-                Endpoint.Browse browseEndpoint = run.getNavigationEndpoint()
-                                                    .getBrowseEndpoint();
-                String pageType = browseEndpoint.getBrowseEndpointContextSupportedConfigs()
-                                                .getBrowseEndpointContextMusicConfig()
-                                                .getPageType();
-
-                // TODO Upgrade to REGEX to better results
-                switch ( pageType ) {
-                    case PageType.ARTIST:
-                        authors.add( browseEndpoint );
-                        break;
-                    case PageType.ALBUM:
-                        album = browseEndpoint;
-                        break;
-                }
-            } catch ( NullPointerException ignored ) {
-            }
-
-        LinkedList<Endpoint.Browse> results = new LinkedList<>( authors );
-        results.addFirst( album );
-
-        // This Deque can't be empty under any circumstances
-        assert !results.isEmpty();
-
-        return results;
-    }
-
-    /**
      * Extract duration text from both columns
      */
     private static @NotNull Optional<String> extractDuration( @NotNull MusicResponsiveListItemRenderer renderer ) {
@@ -146,8 +97,8 @@ public class InnertubeSong implements Identifiable, Visual {
     String authorsText;
 
     public InnertubeSong( PlaylistPanelRenderer.Content.@NotNull VideoRenderer renderer ) {
-        Deque<Endpoint.Browse> artistsAndAlbum = extractArtistsAndAlbum( renderer.getLongBylineText()
-                                                                                 .getRuns() );
+        Deque<Endpoint.Browse> artistsAndAlbum = ItemUtils.extractArtistsAndAlbum( renderer.getLongBylineText()
+                                                                                           .getRuns() );
 
         this.id = renderer.getVideoId();
         this.name = ItemUtils.getFirstText( renderer.getTitle() );
@@ -191,7 +142,7 @@ public class InnertubeSong implements Identifiable, Visual {
         assert titleRun != null;
         assert !runs.isEmpty();
 
-        Deque<Endpoint.Browse> artistsAndAlbum = extractArtistsAndAlbum( runs );
+        Deque<Endpoint.Browse> artistsAndAlbum = ItemUtils.extractArtistsAndAlbum( runs );
 
         this.id = titleRun.getNavigationEndpoint()
                           .getWatchEndpoint()
--- src/main/java/me/knighthat/innertube/model/ItemUtils.java
+++ src/main/java/me/knighthat/innertube/model/ItemUtils.java
@@ -1,11 +1,12 @@
 package me.knighthat.innertube.model;
 
-import me.knighthat.innertube.response.Badge;
-import me.knighthat.innertube.response.Runs;
+import me.knighthat.innertube.PageType;
+import me.knighthat.innertube.response.*;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
 
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 class ItemUtils {
 
@@ -28,6 +29,56 @@ class ItemUtils {
                                                 .getIconType()
                                                 .equals( Badge.EXPLICIT )
                      );
+    }
+
+    /**
+     * Extract album and artists from provided {@link Runs.Run}.
+     * <p>
+     * Album or artist is filtered by their respected {@link PageType}.
+     * <p>
+     * Album's browse will always located at the beginning of the list,
+     * if album browse endpoint doesn't exist in {@code runs}, then
+     * list will be appended with a null {@code null}.
+     * <p>
+     * If the list only contains 1 {@code null } element, it means
+     * the provided {@code runs} contains neither artists nor album
+     *
+     * @param runs set of text and navigation endpoints
+     *
+     * @return album' and artists' browse endpoints in 1 list
+     */
+    static @NotNull LinkedList<Endpoint.Browse> extractArtistsAndAlbum( @NotNull List<? extends Runs.Run> runs ) {
+        List<Endpoint.Browse> authors = new ArrayList<>();
+        Endpoint.Browse album = null;
+
+        for ( Runs.Run run : runs )
+            try {
+                Endpoint.Browse browseEndpoint = run.getNavigationEndpoint()
+                                                    .getBrowseEndpoint();
+                String pageType = browseEndpoint.getBrowseEndpointContextSupportedConfigs()
+                                                .getBrowseEndpointContextMusicConfig()
+                                                .getPageType();
+
+                // TODO Upgrade to REGEX to better results
+                switch ( pageType ) {
+                    case PageType.ARTIST:
+                        authors.add( browseEndpoint );
+                        break;
+                    case PageType.ALBUM:
+                        album = browseEndpoint;
+                        break;
+                }
+            } catch ( NullPointerException ignored ) {
+            }
+
+        // LinkedList supports `null` element
+        LinkedList<Endpoint.Browse> results = new LinkedList<>( authors );
+        results.addFirst( album );
+
+        // This Deque can't be empty under any circumstances
+        assert !results.isEmpty();
+
+        return results;
     }
 // END: Static fields/functions
 }
