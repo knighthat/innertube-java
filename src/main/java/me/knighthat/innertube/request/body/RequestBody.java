5931b1587c5e2cb54d12fce70a9bb8e9a155d769
knighthat <git@knighthat.me>
2025-06-24 23:35:22 -0500

    add method to convert instance to json string

--- src/main/java/me/knighthat/innertube/request/body/RequestBody.java
+++ src/main/java/me/knighthat/innertube/request/body/RequestBody.java
@@ -1,8 +1,17 @@
 package me.knighthat.innertube.request.body;
 
+import com.google.gson.Gson;
 import org.jetbrains.annotations.NotNull;
 
 public interface RequestBody {
 
+// START: Static fields/functions
+    @NotNull Gson GSON = new Gson();
+// END: Static fields/functions
+
     @NotNull Context getContext();
+
+    default @NotNull String toJsonString() {
+        return GSON.toJson( this );
+    }
 }
