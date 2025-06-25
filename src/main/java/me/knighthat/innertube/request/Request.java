f18e037522d3de3d7b025c0f340b92a3a2feae49
knighthat <git@knighthat.me>
2025-06-25 02:28:20 -0500

    add nullable annotation

--- src/main/java/me/knighthat/innertube/request/Request.java
+++ src/main/java/me/knighthat/innertube/request/Request.java
@@ -3,6 +3,9 @@ package me.knighthat.innertube.request;
 import lombok.Builder;
 import lombok.Value;
 import me.knighthat.innertube.request.body.RequestBody;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
 
 import java.util.List;
 import java.util.Map;
@@ -11,8 +14,14 @@ import java.util.Map;
 @Value
 @Builder(builderClassName = "Builder")
 public class Request {
+
+    @NotNull
     String                    httpMethod;
+    @NotNull
     String                    url;
+    @NotNull
+    @Unmodifiable
     Map<String, List<String>> headers;
+    @Nullable       // Should only be null when [httpMethod] is "GET"
     RequestBody               dataToSend;
 }
