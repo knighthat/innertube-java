8517c46408f898eb0236b81ad0b49223acfa3468
knighthat <git@knighthat.me>
2025-06-25 22:42:19 -0500

    add more test cases to InnertubeSong

--- src/test/java/me/knighthat/innertube/model/InnertubeSongTest.java
+++ src/test/java/me/knighthat/innertube/model/InnertubeSongTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
@@ -109,8 +110,8 @@ class InnertubeSongTest {
     }
 
     @Test
-    void testBrowsePlaylistSongMusicResponsiveListItemRendererConstructor() {
-        final String fileName = "ytm/browse/playlist_song_musicResponsiveListItemRenderer.json";
+    void testBrowsePlaylistSongMusicResponsiveListItemRenderer1() {
+        final String fileName = "ytm/browse/playlist_song_musicResponsiveListItemRenderer_1.json";
         Executable executable = () -> {
             try (
                     InputStream inStream = LOADER.getResourceAsStream( fileName ) ;
@@ -136,6 +137,66 @@ class InnertubeSongTest {
                 executable,
                 "failed to instantiate InnertubeSong with " + fileName
         );
+    }
+
+    @Test
+    void testBrowsePlaylistSongMusicResponsiveListItemRenderer2() {
+        final String fileName = "ytm/browse/playlist_song_musicResponsiveListItemRenderer_2.json";
+        try (
+                InputStream inStream = LOADER.getResourceAsStream ( fileName ) ;
+                InputStreamReader reader = new InputStreamReader ( inStream )
+        ) {
+            MusicResponsiveListItemRenderer renderer = GranularParser.musicResponsiveListItemRenderer ( reader );
+
+            InnertubeSong song = InnertubeSong.from ( renderer );
+            Assertions.assertEquals ( "5p_CJiP0KpY", song.getId () );
+            Assertions.assertEquals ( "You Shook Me All Night Long (Live) (duet with Anastacia-Live from VH1 Divas)", song.getName () );
+            Assertions.assertFalse ( song.getThumbnails ().isEmpty () );
+            Assertions.assertEquals ( "3:52", song.getDurationText () );
+            Assertions.assertFalse ( song.isExplicit () );
+
+            // The correct value is "Divas Las Vegas" but since it
+            // doesn't lead to anywhere, might as well not include it
+            Assertions.assertNull ( song.getAlbum () );
+
+            Assertions.assertFalse ( song.getAuthors ().isEmpty () );
+            Assertions.assertNull ( song.getAuthors ().getFirst ().getNavigationEndpoint () );
+            Assertions.assertEquals ( "Céline Dion & Anastacia", song.getAuthorsText () );
+        } catch ( IOException e ) {
+            Assertions.fail ( "Failed to read file %1$s because %2$s".formatted ( fileName, e.getMessage () ) );
+        }
+    }
+
+    @Test
+    void testBrowsePlaylistSongMusicResponsiveListItemRenderer3() {
+        final String fileName = "ytm/browse/playlist_continued_song_musicResponsiveListItemRenderer.json";
+        try (
+                InputStream inStream = LOADER.getResourceAsStream ( fileName ) ;
+                InputStreamReader reader = new InputStreamReader ( inStream )
+        ) {
+            MusicResponsiveListItemRenderer renderer = GranularParser.musicResponsiveListItemRenderer ( reader );
+
+            InnertubeSong song = InnertubeSong.from ( renderer );
+            Assertions.assertEquals ( "Qzb2VcBuA3U", song.getId () );
+            Assertions.assertEquals ( "Clint Eastwood", song.getName () );
+            Assertions.assertFalse ( song.getThumbnails ().isEmpty () );
+            Assertions.assertEquals ( "5:41", song.getDurationText () );
+            Assertions.assertTrue ( song.isExplicit () );
+
+            Assertions.assertNotNull ( song.getAlbum () );
+            Assertions.assertEquals ( "Gorillaz", song.getAlbum ().getText () );
+            Assertions.assertNotNull ( song.getAlbum ().getNavigationEndpoint () );
+            Assertions.assertEquals (
+                    "MPREb_4bUnq6rWRE7",
+                    song.getAlbum ().getNavigationEndpoint ().getBrowseEndpoint ().getBrowseId ()
+            );
+
+            Assertions.assertFalse ( song.getAuthors ().isEmpty () );
+            Assertions.assertNull ( song.getAuthors ().getFirst ().getNavigationEndpoint () );
+            Assertions.assertEquals ( "Gorillaz & Del The Funky Homosapien", song.getAuthorsText () );
+        } catch ( IOException e ) {
+            Assertions.fail ( "Failed to read file %1$s because %2$s".formatted ( fileName, e.getMessage () ) );
+        }
     }
 
     @Test
